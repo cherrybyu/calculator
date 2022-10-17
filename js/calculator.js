bd665c755c47c92a1cf7b14b10dfbb8e2bd3d3e9
hanabyu <cherrybyuu@gmail.com>
2022-10-16 23:08:26 -0700

    add functionality to clear and number buttons

--- js/calculator.js
+++ js/calculator.js
@@ -1,11 +1,17 @@
+const display = document.querySelector('.display')
 const button = document.querySelectorAll('div.button')
+let displayText = document.querySelector('.display-text')
 for (let i = 0; i < button.length; i++) {
+
+    // button hover effects
     button[i].addEventListener('mouseover', function () {
         button[i].classList.add('hover');
     });
     button[i].addEventListener('mouseout', function () {
         button[i].classList.remove('hover');
     });
+
+    // button click effects
     button[i].addEventListener('mousedown', function () {
         button[i].classList.add('clicked')
     })
@@ -13,3 +19,15 @@ for (let i = 0; i < button.length; i++) {
         button[i].classList.remove('clicked')
     })
 }
+
+// displays selected numbers
+const number = document.querySelectorAll('div.number')
+for (let j = 0; j < number.length; j++) {
+    number[j].addEventListener('click', function () {
+        displayText.textContent += number[j].textContent
+    })
+}
+const clear = document.querySelector('#clear')
+clear.addEventListener('click', function () {
+    displayText.textContent = ""
+})
