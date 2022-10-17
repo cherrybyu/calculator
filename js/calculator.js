2dffd02c441b8b4da183126096088f838c853317
hanabyu <cherrybyuu@gmail.com>
2022-10-16 19:32:25 -0700

    add button click effect

--- js/calculator.js
+++ js/calculator.js
@@ -6,4 +6,10 @@ for (let i = 0; i < button.length; i++) {
     button[i].addEventListener('mouseout', function () {
         button[i].classList.remove('hover');
     });
+    button[i].addEventListener('mousedown', function () {
+        button[i].classList.add('clicked')
+    })
+    button[i].addEventListener('mouseup', function () {
+        button[i].classList.remove('clicked')
+    })
 }
